d3215acbd68f10f4d3960280f49c5e1695fe78e7
PriyanshuNimbalkar <195259572+PriyanshuNimbalkar@users.noreply.github.com>
2026-04-16 14:32:46 +0530

    Add solution

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function (string, num) {
+    let total = "";
+  if (num < 0) {
+    return "ERROR";
+  } else {
+    for (let i = num; i > 0; i--) {
+        total += string;
+    }
+    return total;
+  }
 };
 
 // Do not edit below this line
