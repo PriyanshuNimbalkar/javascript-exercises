1d2c6ec9151706c45a81b64b276c3f5b13983259
PriyanshuNimbalkar <195259572+PriyanshuNimbalkar@users.noreply.github.com>
2026-04-16 21:13:49 +0530

    Add solution

--- foundations/07_reverseString/reverseString.js
+++ foundations/07_reverseString/reverseString.js
@@ -1,5 +1,13 @@
-const reverseString = function() {
+const reverseString = function (str) {
+  //   const reverse = str.split("").reverse().join("");
+  //   return reverse;
 
+  let reverse = "";
+
+  for (let i = str.length - 1; i >= 0; i--) {
+    reverse += str[i];
+  }
+  return reverse;
 };
 
 // Do not edit below this line
