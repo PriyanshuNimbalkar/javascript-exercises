884d78ecb8c97fefa3324fbac70fbe856d51598d
PriyanshuNimbalkar <195259572+PriyanshuNimbalkar@users.noreply.github.com>
2026-04-22 21:05:59 +0530

    Add solution

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,5 +1,14 @@
-const sumAll = function() {
-
+const sumAll = function (...val) {
+  if (val.some((a) => typeof a !== "number" && !isNaN(a))) {
+    return "ERROR";
+  }
+  if (val.some((a) => a < 0 || !Number.isInteger(a))) return "ERROR";
+  val.sort((a, b) => a - b);
+  let total = 0;
+  for (let i = val[0]; i <= val[val.length - 1]; i++) {
+    total += i;
+  }
+  return total;
 };
 
 // Do not edit below this line
