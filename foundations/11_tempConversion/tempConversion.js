0da0431f51527282852a2ba2139bb1cba84e5b7f
PriyanshuNimbalkar <195259572+PriyanshuNimbalkar@users.noreply.github.com>
2026-04-24 06:56:48 +0530

    Add solution

--- foundations/11_tempConversion/tempConversion.js
+++ foundations/11_tempConversion/tempConversion.js
@@ -1,11 +1,17 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (t) {
+  return parseFloat((((t - 32) * 5) / 9).toFixed(1));
 };
 
-const convertToFahrenheit = function() {
+console.log(convertToCelsius(100));
+
+const convertToFahrenheit = function (t) {
+  return parseFloat((t * 1.8 + 32).toFixed(1));
 };
+
+console.log(convertToFahrenheit(0));
 
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
-  convertToFahrenheit
+  convertToFahrenheit,
 };
